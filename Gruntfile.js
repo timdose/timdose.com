c3414762c0ead158d525d13becc8c3a75192047d
Tim Dose <timdose@gmail.com>
2014-03-29 14:41:46 -0400

    added resume

--- Gruntfile.js
+++ Gruntfile.js
@@ -346,7 +346,8 @@ module.exports = function (grunt) {
                         'images/{,*/}*.webp',
                         '{,*/}*.html',
                         'styles/fonts/{,*/}*.*',
-                        'bower_components/bootstrap/fonts/*.*'
+                        'bower_components/bootstrap/fonts/*.*',
+                        'assets/**/*'
                     ]
                 }]
             },
