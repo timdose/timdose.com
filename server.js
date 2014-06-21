957d765c583b2c597b78f134b5ee5a492e527567
Tim Dose <tdose@yodle.com>
2014-06-21 15:21:33 -0400

    logger for express

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 var express = require('express');
+var logger = require('morgan');
+
 var controllers = require('./app/controllers');
 
 var app = express();
 module.exports = app; // expose for testing
 
+var logMode = 'default';
+if ( app.get('env') == 'development' ) logMode = 'dev';
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/app/views');
 app.set('view engine', 'jade');
 
+app.use(logger(logMode));
 app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', controllers.index );
 
