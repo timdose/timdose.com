46ad76d93a0b030f57f8619251ed519000690227
Tim Dose <tdose@yodle.com>
2014-06-23 20:25:04 -0400

    livereload for server

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var livereload = require('gulp-livereload');
 var nodemon = require('gulp-nodemon');
 
+var lrServer;
+
 var paths = {
   frontEnd: [
               'app/data/**/*.yaml', 
@@ -11,13 +13,17 @@ var paths = {
             ]
 }
 
+
 gulp.task('develop', function () {
   nodemon({ script: 'server.js', ext: 'html js', ignore: ['ignored.js'] })
-    // .on('change', ['lint'])
     .on('restart', function () {
       console.log('restarted!')
+      setTimeout(function() {
+        livereload.changed();
+      }, 2000);
     })
 })
+
 
 gulp.task('watch', function() {
   livereload.listen();
