e4bd2d18afd10520644f71fe439dcc16b4b09d47
Tim Dose <tdose@yodle.com>
2014-06-27 19:11:27 -0400

    ported fine art portfolio

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,11 +4,12 @@ var fs = require('fs');
 var data = { 
   app: yaml.safeLoad(fs.readFileSync('app/data/app-data.yaml', 'utf8')),
   about: yaml.safeLoad(fs.readFileSync('app/data/about.yaml', 'utf8')),
-  projects: yaml.safeLoad(fs.readFileSync('app/data/projects.yaml', 'utf8'))
+  projects: yaml.safeLoad(fs.readFileSync('app/data/projects.yaml', 'utf8')),
+  art: yaml.safeLoad(fs.readFileSync('app/data/art.yaml', 'utf8'))
 }
 
 exports.index = function(req, res) {
-  res.render('index', data );
+  res.render('index', {portfolio:data.art} );
 };
 
 exports.resume = function(req, res) {
