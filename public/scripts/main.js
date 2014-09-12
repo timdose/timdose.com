cfa061cf4fd7a218ffcf97da236bf7b8a2df0c26
Tim Dose <tdose@yodle.com>
2014-09-11 23:49:26 -0400

    highlight current page in nav

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,44 +2,19 @@
 
 
 $(document).ready(function(){
-  stickyNav();
+  highlightCurrentPage();
 });
 
-// from http://www.bluthemes.com/blog/7/dynamic-bootstrap-affix-fix
-function stickyNav() {
-  var stickyElement = '#scroll-nav', // the element you want to make sticky
-      bottomElement = '.footer'; // the bottom element where you want the sticky element to stop (usually the footer)
-  
-  // make sure the element exists on the page before trying to initalize
-  if($( stickyElement ).length){
-    $( stickyElement ).each(function(){
-
-      // let's save some messy code in clean variables
-      // when should we start affixing? (the amount of pixels to the top from the element)
-      var fromTop = $( this ).offset().top,
-          // where is the bottom of the element?
-          fromBottom = $( document ).height()-($( this ).offset().top + $( this ).outerHeight()),
-          // where should we stop? (the amount of pixels from the top where the bottom element is)
-          // also add the outer height mismatch to the height of the element to account for padding and borders
-          stopOn = $( document ).height()-( $( bottomElement ).offset().top)+($( this ).outerHeight() - $( this ).height());
-      
-      // if the element doesn't need to get sticky, then skip it so it won't mess up your layout
-      if( (fromBottom-stopOn) > 200 ){
-        // let's put a sticky width on the element and assign it to the top
-        $( this ).css('width', $( this ).width()).css('top', 0).css('position', '');
-        // assign the affix to the element
-        $( this ).affix({
-          offset: {
-            // make it stick where the top pixel of the element is
-            top: fromTop,
-            // make it stop where the top pixel of the bottom element is
-            bottom: stopOn
-          }
-        // when the affix get's called then make sure the position is the default (fixed) and it's at the top
-        }).on('affix.bs.affix', function(){ $( this ).css('top', 0).css('position', ''); });
-      }
-      // trigger the scroll event so it always activates
-      $( window ).trigger('scroll');
-    });
-  }
+function highlightCurrentPage() {
+  var currentPage = window.location.pathname;
+  $('.navbar-nav a').each(function() {
+    var link = $(this).attr('href');
+    if ( currentPage == link ) { 
+      $(this).parent('li').addClass('active');
+      var targetName = $(this).data('activate');
+      var selector = '*[data-active="'+targetName+'"]';
+      var target = $(selector);
+      $(target).addClass('active');
+    }
+  });
 }
